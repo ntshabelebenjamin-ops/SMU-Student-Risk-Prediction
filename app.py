b6bd55718cd8962d7654329546ae8919af48ba9d
ntshabelebenjamin-ops <ntshabelebenjamin@gmail.com>
2026-05-29 14:00:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ col1, col2, col3 = st.columns(3)
 col1.metric("First-Time Entering Students", len(df))
 col2.metric("Variables", len(df.columns))
 col3.metric("Missing Values", int(df.isnull().sum().sum()))
+demographic_vars = [
+    "3. What was the main language used by teachers in class?",
+    "5. Was your language learning at school the same as your home language?",
+    "6. Is English your first, second or third language?",
+    "7. How did you pay for your school fees?",
+    "8. What was the average number of learners in your matric classroom?",
+    "9. How would you describe the place in which your school is situated?"
+]
